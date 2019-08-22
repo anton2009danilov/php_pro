a07346515f874e33b59390dec68f8b52ea1a20e7
Anton Danilov <anton2009danilov@yandex.ru>
2019-08-22 11:30:23 +0300

    исправление в классе Autoload

--- services/Autoload.php
+++ services/Autoload.php
@@ -5,12 +5,14 @@ class Autoload
 {
 
     public function loadClass($className) {
-//         var_dump($className);
-//         var_dump(substr_replace($className, '', 0, 4));
-        $str = substr_replace($className, '', 0, 4);
-//         var_dump(dirname(__DIR__));
+//         var_dump(explode('\\', $className, 2));
+        $directory = explode('\\', $className, 2)[0];
+        if ($directory == 'App') {
+            $name = substr_replace($className, '', 0, 4);
+//             var_dump($name);
+        }
+            $file = dirname(__DIR__) . "/{$name}.php";
 //         $file = dirname(__DIR__) . "/{$className}.php";
-        $file = dirname(__DIR__) . "/{$str}.php";
         include $file;
     }
     
